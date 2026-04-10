dd12d3b957e24c37e09543c1a88dda3dc007f515
aviral-beehyv <aviral.gupta@beehyv.com>
2026-04-11 00:52:58 +0530

    remove unused MockitoExtension from LocationHierarchyStructureLockedExceptionTest

--- backend/tenant-service/src/test/java/org/arghyam/jalsoochak/tenant/exception/LocationHierarchyStructureLockedExceptionTest.java
+++ backend/tenant-service/src/test/java/org/arghyam/jalsoochak/tenant/exception/LocationHierarchyStructureLockedExceptionTest.java
@@ -2,12 +2,9 @@ package org.arghyam.jalsoochak.tenant.exception;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
 @DisplayName("LocationHierarchyStructureLockedException")
 class LocationHierarchyStructureLockedExceptionTest {
 
